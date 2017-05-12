656fd8711ceb0ff56bfe6425aaf4436a65b5a625
Aleksander Leszczyński <leszczynski.al@gmail.com>
2017-05-12 10:43:04 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express"),
+    app = express(),
+    moment = require("moment"),
+    dateFormats = [
+        "MMDDYYYY",
+        "YYYYMMDD",
+        "MMMMDDYYYY",
+        "DDMMMMYYYY",
+        "DDYYYYMMMM",
+        ];
+app.set("view engine", "ejs");        
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get("/:time", function(req, res) {
+    var response = {
+        unix: null,
+        natural: null
+    };
+    var input = req.params.time;
+    if(/^\d+$/.test(input)) {
+        response.unix = input;
+        response.natural = moment.unix(input).format("MMMM DD, YYYY");
+    } else if(moment(input, dateFormats).isValid()) {
+        var date = moment(input, dateFormats);
+        response.natural = date.format("MMMM DD, YYYY");
+        response.unix = moment(date).unix();
+    }
+    res.send(response);
+    
+    //console.log(moment.unix(input).format("MMMM DD, YYYY"));
+    
+});
+
+app.listen(process.env.PORT, process.env.ID, function() {
+    console.log("Server is listening");
+});
